0cbc838e4e40889bf02af2d533501b9bdf14a534
Konstantin Arakantsev <kostya3312@gmail.com>
2020-01-04 21:51:18 +0300

    Add getPosts and createPost cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
+require("dotenv").config();
+
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello Socialape!");
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then(snap => {
+      const posts = snap.docs.map(doc => doc.data());
+      return res.json(posts);
+    })
+    .catch(err => console.log(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method is not allowed" });
+  }
+
+  const { userHandle, body } = req.body;
+
+  const newPost = {
+    userHandle,
+    body,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then(doc => {
+      return res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: "Something went worng" });
+    });
 });
